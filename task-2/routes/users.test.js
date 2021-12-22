718d997cccb85fa667353e4489b37334d777be08
Lorcan Mc <lorcan.jmc@gmail.com>
2021-12-22 13:50:18 +0000

    task 2 started

--- task-2/routes/users.test.js
+++ task-2/routes/users.test.js
@@ -1,1 +1,30 @@
+import request from "supertest";
+
+import app from "../app.js";
+
 // Write your tests for task 2 in this file. Plan out what you need to import/require.
+
+
+request(app)
+    .get("/")
+    .something
+    .something
+
+
+
+
+
+
+
+
+
+
+    // EXAMPLE:
+// request(app)
+// .get('/user')
+// .expect('Content-Type', /json/)
+// .expect('Content-Length', '15')
+// .expect(200)
+// .end(function(err, res) {
+//     if (err) throw err;
+// });
